3379880258c711fcec92f50d9e63579458259642
Nate Bird <me@Nates-MacBook.local>
2008-11-24 16:36:39 -0500

    Updated login view
    Updated Operator layout with code from directory
    
    Added stylesheets from directory app

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function jumpToArea(sel) {
+	if (sel.selectedIndex < 1) return;
+	area = sel.options[sel.selectedIndex].value;
+	
+	if (document.location.pathname.match(/^\/areas\/\d+/)) {
+		document.location.href = document.location.pathname.replace(/\d+/, area)
+	} else {
+		document.location.href = '/operator/areas/' + area
+	}
+}
