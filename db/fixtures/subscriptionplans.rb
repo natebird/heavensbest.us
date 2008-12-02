32c6d08b2b06327483d1b9f40530b9cf2749e561
Nate Bird <me@Nates-MacBook.local>
2008-12-02 12:46:07 -0500

    Changed Subscription Plans to monthly plans. Need to repopulate the database.

--- db/fixtures/subscriptionplans.rb
+++ db/fixtures/subscriptionplans.rb
@@ -1,50 +1,53 @@
-# SubscriptionPlan.seed do |s|
-#   s.id = 1
-#   s.name = "Basic"
-#   s.amount = 10    
-#   s.renewal_period = 1
-#   s.trial_period = 1
-# end
-
 SubscriptionPlan.seed do |s|
-  s.id = 2
+  s.id = 1
   s.name = "Basic"
-  s.amount = 100    
-  s.renewal_period = 12
+  s.amount = 10    
+  s.renewal_period = 1
   s.trial_period = nil
   s.special_limit = 0
 end
 
 # SubscriptionPlan.seed do |s|
-#   s.id = 3
-#   s.name = "Plus"
-#   s.amount = 20    
-#   s.renewal_period = 1
-#   s.trial_period = 1
+#   s.id = 2
+#   s.name = "Basic"
+#   s.amount = 100    
+#   s.renewal_period = 12
+#   s.trial_period = nil
+#   s.special_limit = 0
 # end
 
 SubscriptionPlan.seed do |s|
-  s.id = 4
+  s.id = 3
   s.name = "Plus"
-  s.amount = 200    
-  s.renewal_period = 12
+  s.amount = 20    
+  s.renewal_period = 1
   s.trial_period = nil
   s.special_limit = 1
 end
 
 # SubscriptionPlan.seed do |s|
-#   s.id = 5
-#   s.name = "Premium"
-#   s.amount = 30    
-#   s.renewal_period = 1
-#   s.trial_period = 1
+#   s.id = 4
+#   s.name = "Plus"
+#   s.amount = 200    
+#   s.renewal_period = 12
+#   s.trial_period = nil
+#   s.special_limit = 1
 # end
 
 SubscriptionPlan.seed do |s|
-  s.id = 6
+  s.id = 5
   s.name = "Premium"
-  s.amount = 300    
-  s.renewal_period = 12
+  s.amount = 30    
+  s.renewal_period = 1
   s.trial_period = nil
   s.special_limit = 20
 end
+
+# SubscriptionPlan.seed do |s|
+#   s.id = 6
+#   s.name = "Premium"
+#   s.amount = 300    
+#   s.renewal_period = 12
+#   s.trial_period = nil
+#   s.special_limit = 20
+# end
