11fcd5885d4fcfde3069af2d836554294886373d
Nate Bird <me@Nates-MacBook.local>
2008-12-05 18:08:48 -0500

    Updated the helper function for cancel_or_submit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,23 +18,25 @@ module ApplicationHelper
   end
 
   # Sets a link with a class of current when the url is matched
-  def layout_link_to(link_text, path, extra_class)
-    curl = url_for(:controller => request.path_parameters['controller'],
-                          :action => request.path_parameters['action'])
+  def layout_link_to(link_text, path)
+    curl = url_for(:controller => request.path_parameters['controller'], :action => request.path_parameters['action'])
+    html = ''
+    options = path == curl ? {:class => 'current'} : {}
+    html << content_tag("li", link_to(link_text, path, options))
+  end
+
+  # Creates a list of links and sets the css class to current when the url is matches  
+  def tab_for(link_text, path, extra_class)
+    curl = url_for(:controller => request.path_parameters['controller'], :action => request.path_parameters['action'])
     html = ''
     options = path == curl ? {:class => 'current', :id => extra_class} : {:id => extra_class}
     html << content_tag("li", link_to(link_text, path, options))
   end
 
-  # Creates a list of links and sets the css class to current when the url is matches  
-  def tab_for(tab, link, label=nil)
-    content_tag :li, link_to(label||tab.to_s.titleize, link), :class => ("current_tab" if @current_tab == tab)
-  end
-
   # Render a submit button and cancel link
   def submit_or_cancel(cancel_url = session[:return_to] ? session[:return_to] : url_for(:action => 'index'), label = 'Save Changes')
-    content_tag(:div, submit_tag(label) + ' or ' +
-      link_to('Cancel', cancel_url), :id => 'submit_or_cancel', :class => 'submit')
+    content_tag(:div, link_to('Cancel', cancel_url) + ' or ' + submit_tag(label), 
+    :id => 'submit_or_cancel', :class => 'submit')
   end
   
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -8,7 +8,7 @@ class Account < ActiveRecord::Base
   has_many :services
   has_many :specials
   has_many :testimonials
-  # has_many :tips
+  has_many :tickets
   
   validates_uniqueness_of :name
   validates_presence_of :email
