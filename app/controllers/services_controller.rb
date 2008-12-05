f1f2ed5fea673643ebf878d2d4edd77d966fa320
Nate Bird <me@Nates-MacBook.local>
2008-12-05 18:07:52 -0500

    Updated services routes under an area. They now show the name of the service in the URL

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,11 +2,12 @@ class ServicesController < ApplicationController
 
   def index
     @services = current_account.services.find(:all)
+    redirect_to account_service_path(@account, @services.first.title)
   end
 
 
   def show
-    @service = current_account.services.find(params[:id])
+    @service = current_account.services.find_by_title(params[:id])
     @services = current_account.services.find(:all)
   end
 
