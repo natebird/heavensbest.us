b1cc23f32a29a2f6ff845757e26176a8eeff4057
Nate Bird <me@Nates-MacBook.local>
2008-12-18 23:11:58 -0500

    Changes to account and service validations.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ActiveRecord::Base
   belongs_to :account
   belongs_to :photo
+  
+  validates_length_of :title, :maximum=>22, :message=>"less than {{count}} or it won't fit"
+  validates_presence_of :photo_id, :body, :title
 end
