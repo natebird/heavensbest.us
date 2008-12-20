df2b10a8011015d7acf3c69c3e3575db4920c254
Nate Bird <me@Nates-MacBook.local>
2008-12-20 17:19:18 -0500

    A bunch of commented out portions of the fixtures files for seeding to the db. Still not entirely sure what is wrong.

--- db/fixtures/regions.rb
+++ db/fixtures/regions.rb
@@ -1,357 +1,357 @@
-# US States
-Region.seed do |s|
-  s.id = 1
-  s.name = "Alabama"
-  s.abbreviation = "AL"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 2
-  s.name = "Alaska"
-  s.abbreviation = "AK"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 3
-  s.name = "Arizona"
-  s.abbreviation = "AZ"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 4
-  s.name = "Arkansas"
-  s.abbreviation = "AR"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 5
-  s.name = "California"
-  s.abbreviation = "CA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 6
-  s.name = "Colorado"
-  s.abbreviation = "CO"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 7
-  s.name = "Connecticut"
-  s.abbreviation = "CT"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 8
-  s.name = "Delaware"
-  s.abbreviation = "DE"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 9
-  s.name = "District of Columbia"
-  s.abbreviation = "DC"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 10
-  s.name = "Florida"
-  s.abbreviation = "FL"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 11
-  s.name = "Georgia"
-  s.abbreviation = "GA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 12
-  s.name = "Hawaii"
-  s.abbreviation = "HI"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 13
-  s.name = "Idaho"
-  s.abbreviation = "ID"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 14
-  s.name = "Illinois"
-  s.abbreviation = "IL"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 15
-  s.name = "Indiana"
-  s.abbreviation = "IN"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 16
-  s.name = "Iowa"
-  s.abbreviation = "IA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 17
-  s.name = "Kansas"
-  s.abbreviation = "KS"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 18
-  s.name = "Kentucky"
-  s.abbreviation = "KY"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 19
-  s.name = "Louisiana"
-  s.abbreviation = "LA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 20
-  s.name = "Maine"
-  s.abbreviation = "ME"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 21
-  s.name = "Maryland"
-  s.abbreviation = "MD"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 22
-  s.name = "Massachusetts"
-  s.abbreviation = "MA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 23
-  s.name = "Michigan"
-  s.abbreviation = "MI"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 24
-  s.name = "Minnesota"
-  s.abbreviation = "MN"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 25
-  s.name = "Mississippi"
-  s.abbreviation = "MS"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 26
-  s.name = "Missouri"
-  s.abbreviation = "MO"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 27
-  s.name = "Montana"
-  s.abbreviation = "MT"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 28
-  s.name = "Nebraska"
-  s.abbreviation = "NE"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 29
-  s.name = "Nevada"
-  s.abbreviation = "NV"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 30
-  s.name = "New Hampshire"
-  s.abbreviation = "NH"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 31
-  s.name = "New Jersey"
-  s.abbreviation = "NJ"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 32
-  s.name = "New Mexico"
-  s.abbreviation = "NM"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 33
-  s.name = "New York"
-  s.abbreviation = "NY"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 34
-  s.name = "North Carolina"
-  s.abbreviation = "NC"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 35
-  s.name = "North Dakota"
-  s.abbreviation = "ND"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 36
-  s.name = "Ohio"
-  s.abbreviation = "OH"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 37
-  s.name = "Oklahoma"
-  s.abbreviation = "OK"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 38
-  s.name = "Oregon"
-  s.abbreviation = "OR"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 39
-  s.name = "Pennsylvania"
-  s.abbreviation = "PA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 40
-  s.name = "Rhode Island"
-  s.abbreviation = "RI"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 41
-  s.name = "South Carolina"
-  s.abbreviation = "SC"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 42
-  s.name = "South Dakota"
-  s.abbreviation = "SD"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 43
-  s.name = "Tennessee"
-  s.abbreviation = "TN"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 44
-  s.name = "Texas"
-  s.abbreviation = "TX"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 45
-  s.name = "Utah"
-  s.abbreviation = "UT"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 46
-  s.name = "Vermont"
-  s.abbreviation = "VT"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 47
-  s.name = "Virginia"
-  s.abbreviation = "VA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 48
-  s.name = "Washington"
-  s.abbreviation = "WA"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 49
-  s.name = "West Virginia"
-  s.abbreviation = "WV"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 50
-  s.name = "Wisconsin"
-  s.abbreviation = "WI"
-  s.country_id = 1
-end
-
-Region.seed do |s|
-  s.id = 51
-  s.name = "Wyoming"
-  s.abbreviation = "WY"
-  s.country_id = 1
-end
+# # US States
+# Region.seed do |s|
+#   s.id = 1
+#   s.name = "Alabama"
+#   s.abbreviation = "AL"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 2
+#   s.name = "Alaska"
+#   s.abbreviation = "AK"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 3
+#   s.name = "Arizona"
+#   s.abbreviation = "AZ"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 4
+#   s.name = "Arkansas"
+#   s.abbreviation = "AR"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 5
+#   s.name = "California"
+#   s.abbreviation = "CA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 6
+#   s.name = "Colorado"
+#   s.abbreviation = "CO"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 7
+#   s.name = "Connecticut"
+#   s.abbreviation = "CT"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 8
+#   s.name = "Delaware"
+#   s.abbreviation = "DE"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 9
+#   s.name = "District of Columbia"
+#   s.abbreviation = "DC"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 10
+#   s.name = "Florida"
+#   s.abbreviation = "FL"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 11
+#   s.name = "Georgia"
+#   s.abbreviation = "GA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 12
+#   s.name = "Hawaii"
+#   s.abbreviation = "HI"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 13
+#   s.name = "Idaho"
+#   s.abbreviation = "ID"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 14
+#   s.name = "Illinois"
+#   s.abbreviation = "IL"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 15
+#   s.name = "Indiana"
+#   s.abbreviation = "IN"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 16
+#   s.name = "Iowa"
+#   s.abbreviation = "IA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 17
+#   s.name = "Kansas"
+#   s.abbreviation = "KS"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 18
+#   s.name = "Kentucky"
+#   s.abbreviation = "KY"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 19
+#   s.name = "Louisiana"
+#   s.abbreviation = "LA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 20
+#   s.name = "Maine"
+#   s.abbreviation = "ME"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 21
+#   s.name = "Maryland"
+#   s.abbreviation = "MD"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 22
+#   s.name = "Massachusetts"
+#   s.abbreviation = "MA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 23
+#   s.name = "Michigan"
+#   s.abbreviation = "MI"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 24
+#   s.name = "Minnesota"
+#   s.abbreviation = "MN"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 25
+#   s.name = "Mississippi"
+#   s.abbreviation = "MS"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 26
+#   s.name = "Missouri"
+#   s.abbreviation = "MO"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 27
+#   s.name = "Montana"
+#   s.abbreviation = "MT"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 28
+#   s.name = "Nebraska"
+#   s.abbreviation = "NE"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 29
+#   s.name = "Nevada"
+#   s.abbreviation = "NV"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 30
+#   s.name = "New Hampshire"
+#   s.abbreviation = "NH"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 31
+#   s.name = "New Jersey"
+#   s.abbreviation = "NJ"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 32
+#   s.name = "New Mexico"
+#   s.abbreviation = "NM"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 33
+#   s.name = "New York"
+#   s.abbreviation = "NY"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 34
+#   s.name = "North Carolina"
+#   s.abbreviation = "NC"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 35
+#   s.name = "North Dakota"
+#   s.abbreviation = "ND"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 36
+#   s.name = "Ohio"
+#   s.abbreviation = "OH"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 37
+#   s.name = "Oklahoma"
+#   s.abbreviation = "OK"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 38
+#   s.name = "Oregon"
+#   s.abbreviation = "OR"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 39
+#   s.name = "Pennsylvania"
+#   s.abbreviation = "PA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 40
+#   s.name = "Rhode Island"
+#   s.abbreviation = "RI"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 41
+#   s.name = "South Carolina"
+#   s.abbreviation = "SC"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 42
+#   s.name = "South Dakota"
+#   s.abbreviation = "SD"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 43
+#   s.name = "Tennessee"
+#   s.abbreviation = "TN"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 44
+#   s.name = "Texas"
+#   s.abbreviation = "TX"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 45
+#   s.name = "Utah"
+#   s.abbreviation = "UT"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 46
+#   s.name = "Vermont"
+#   s.abbreviation = "VT"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 47
+#   s.name = "Virginia"
+#   s.abbreviation = "VA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 48
+#   s.name = "Washington"
+#   s.abbreviation = "WA"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 49
+#   s.name = "West Virginia"
+#   s.abbreviation = "WV"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 50
+#   s.name = "Wisconsin"
+#   s.abbreviation = "WI"
+#   s.country_id = 1
+# end
+# 
+# Region.seed do |s|
+#   s.id = 51
+#   s.name = "Wyoming"
+#   s.abbreviation = "WY"
+#   s.country_id = 1
+# end
