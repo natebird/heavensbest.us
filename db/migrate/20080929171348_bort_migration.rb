251c350dec98cc028a6a701f401e54ec8dcb84b2
Nate Bird <me@Nates-MacBook.local>
2008-12-20 16:29:57 -0500

    Updated bort migration. Removed admin creation because mysql didn't like it. Added a setup.rake file with the admin creation stuff. I'll use this information in the console. Maybe some day it will be a real rake task.

--- db/migrate/20080929171348_bort_migration.rb
+++ db/migrate/20080929171348_bort_migration.rb
@@ -60,24 +60,6 @@ class BortMigration < ActiveRecord::Migration
       t.belongs_to :role
       t.belongs_to :user
     end
-    
-    # Create admin role
-    admin_role = Role.create(:name => 'admin')
-    
-    # Create default admin user
-    user = User.create do |u|
-      u.login = 'nate'
-      u.password = u.password_confirmation = '123456'
-      u.email = APP_CONFIG[:admin_email]
-      u.name = 'Nate Bird'
-    end
-    
-    # Activate user
-    user.register!
-    user.activate!
-    
-    # Add admin role to admin user
-    user.roles << admin_role
   end
 
   def self.down
