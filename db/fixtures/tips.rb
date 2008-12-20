36ee66930ba90d856880eed2cca94770bb5ef604
Nate Bird <me@Nates-MacBook.local>
2008-12-20 17:21:13 -0500

    Uncommented out tips fixture

--- db/fixtures/tips.rb
+++ db/fixtures/tips.rb
@@ -1,14 +1,14 @@
 # Tip seed data
-# Tip.seed do |s|
-#   s.id = 1
-#   s.title = "Protect Your Carpet"
-#   s.body = ""
-#   s.active = 1
-# end
-# 
-# Tip.seed do |s|
-#   s.id = 2
-#   s.title = "Carpet Care Tips"
-#   s.body = ""
-#   s.active = 1
-# end
+Tip.seed do |s|
+  s.id = 1
+  s.title = "Protect Your Carpet"
+  s.body = "Do this and this and this"
+  s.active = 1
+end
+
+Tip.seed do |s|
+  s.id = 2
+  s.title = "Carpet Care Tips"
+  s.body = "Do this and this and this"
+  s.active = 1
+end
