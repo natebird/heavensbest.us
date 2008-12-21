c04f300d105c846adc13be9c87e1896400794842
Nate Bird <me@Nates-MacBook.local>
2008-12-20 22:10:34 -0500

    Removed will_paginate and RedCloth gems from vendor directory. Cleaned up environment files for production.

--- config/environment.rb
+++ config/environment.rb
@@ -29,7 +29,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
-  config.gem "lockfile"
+  config.gem 'lockfile'
   
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
@@ -77,4 +77,3 @@ Rails::Initializer.run do |config|
 end
 
 require 'association_proxy'
-require 'RedCloth'
