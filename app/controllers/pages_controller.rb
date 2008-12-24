35054c287da0cf74a18ea71c8c8a29ffb174b39b
Nate Bird <me@Nates-MacBook.local>
2008-12-24 11:15:12 -0500

    Updated Pages controller to render a specific layout based on the category (application or operator).

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
-  layout "operator"
 
   def show
     if params[:permalink]
       @page = Page.find_by_permalink(params[:permalink])
       raise ActiveRecord::RecordNotFound, "Page not found" if @page.nil?
+      render :layout => @page.category
     else
       @page = Page.find(params[:id])
     end
