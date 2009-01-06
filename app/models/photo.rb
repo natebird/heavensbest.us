0afa9918fd94f8ea2a348df2cc3639ad5bc4c822
Nate Bird <me@Nates-MacBook.local>
2009-01-06 16:20:25 -0500

    Updates to the photo model and the location of the photos for services.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
   has_many :services
   
-  has_attached_file :image
+  has_attached_file :image, :url => "/images/services/:attachment/:id_:style.:extension",
+                            :path => ":rails_root/public/images/services/:attachment/:id_:style.:extension"
+  
 end
