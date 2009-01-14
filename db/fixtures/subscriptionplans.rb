4e2889be617d86c937b70b3114df967f0c9069e2
Nate Bird <me@Nates-MacBook.local>
2009-01-14 11:40:54 -0500

    Remove fixtures

--- db/fixtures/subscriptionplans.rb
+++ db/fixtures/subscriptionplans.rb
@@ -1,11 +1,11 @@
-SubscriptionPlan.seed do |s|
-  s.id = 1
-  s.name = "Premium"
-  s.amount = 30    
-  s.renewal_period = 1
-  s.trial_period = nil
-  s.special_limit = 20
-end
+# SubscriptionPlan.seed do |s|
+#   s.id = 1
+#   s.name = "Premium"
+#   s.amount = 30    
+#   s.renewal_period = 1
+#   s.trial_period = nil
+#   s.special_limit = 20
+# end
 
 # SubscriptionPlan.seed do |s|
 #   s.id = 2
