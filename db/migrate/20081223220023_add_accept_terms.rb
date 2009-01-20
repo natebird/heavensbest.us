910e46ad775404c305a30904a15f4676a12713e3
Nate Bird <nate@lucidi.us>
2009-01-20 14:35:41 -0500

    Updated subscription plan information and fixed migration for accept.

--- db/migrate/20081223220023_add_accept_terms.rb
+++ db/migrate/20081223220023_add_accept_terms.rb
@@ -1,9 +1,9 @@
 class AddAcceptTerms < ActiveRecord::Migration
   def self.up
-    add_column :account, :accept, :integer
+    add_column :accounts, :accept, :integer
   end
 
   def self.down
-    drop_column :account, :accept
+    drop_column :accounts, :accept
   end
 end
