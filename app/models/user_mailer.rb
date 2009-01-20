c0bef83d226111dd70f58d685513c131cc3d6889
Nate Bird <nate@lucidi.us>
2009-01-20 15:55:47 -0500

    Updated to the welcome email text and removed the activation email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,14 +1,11 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject << 'An invitation to establish your online presence'
+    @subject << 'Invitation to www.HeavensBest.us'
     @body[:url] = "#{APP_CONFIG[:site_url]}/signup/#{user.activation_code}"
   end
   
   def activation(user)
-    setup_email(user)
-    @subject << 'Your account has been activated!'
-    @body[:url] = APP_CONFIG[:site_url]
   end
   
   protected
