0a49fc0aae59fbad9692dc96aeb55ed2bc2510b4
Nate Bird <nate@lucidi.us>
2009-01-20 14:39:43 -0500

    Fixes to new area form.

--- db/fixtures/tips.rb
+++ db/fixtures/tips.rb
@@ -1,13 +1,34 @@
 # Tip seed data
 Tip.seed do |s|
   s.id = 1
-  s.title = "Protect Your Carpet"
-  s.body = "Do this and this and this"
+  s.title = "Carpet Lifespan"
+  s.body = "Cleaning your carpets every 6 to 12 months doubles the life of your carpet"
   s.active = 1
 end
 
 Tip.seed do |s|
   s.id = 2
+  s.title = "Carpet Care Tips"
+  s.body = "Do this and this and this"
+  s.active = 1
+end
+
+Tip.seed do |s|
+  s.id = 3
+  s.title = "Carpet Care Tips"
+  s.body = "Do this and this and this"
+  s.active = 1
+end
+
+Tip.seed do |s|
+  s.id = 4
+  s.title = "Carpet Care Tips"
+  s.body = "Do this and this and this"
+  s.active = 1
+end
+
+Tip.seed do |s|
+  s.id = 5
   s.title = "Carpet Care Tips"
   s.body = "Do this and this and this"
   s.active = 1
