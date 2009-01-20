0ab791c80d271b12bcc1aa7778e02fda50ae8f54
Nate Bird <nate@lucidi.us>
2009-01-20 10:26:22 -0500

    Updated the location of uploaded photos.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   has_many :services
   
-  has_attached_file :image, :url => "/images/services/:attachment/:id_:style.:extension",
-                            :path => ":rails_root/public/images/services/:attachment/:id_:style.:extension"
+  has_attached_file :image, :url => "/system/images/:attachment/:id_:style.:extension",
+                            :path => ":rails_root/public/system/images/:attachment/:id_:style.:extension"
   
 end
