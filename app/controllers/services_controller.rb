7a4dde38a65ab01a6d66cfddf50a12310b0deffc
Nate Bird <nate@lucidi.us>
2009-01-20 22:18:35 -0500

    Fixed services publishing bug.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,7 @@
 class ServicesController < ApplicationController
 
   def index
-    @services = current_account.services.find(:all, :conditions => [ "active = ?", "t" ] )
+    @services = current_account.services.find(:all, :conditions => [ "active = ?", true ] )
     @region = @account.region.abbreviation.downcase
     redirect_to service_path(@region, @account.accountlink, @services.first.servicelink)
   end
@@ -9,7 +9,7 @@ class ServicesController < ApplicationController
 
   def show
     @service = current_account.services.find_by_servicelink(params[:id])
-    @services = current_account.services.find(:all, :conditions => [ "active = ?", "t" ] )
+    @services = current_account.services.find(:all, :conditions => [ "active = ?", true ] )
   end
 
 end
