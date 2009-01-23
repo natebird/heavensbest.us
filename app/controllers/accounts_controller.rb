6384fddfdda420b4692f9d155ec5130925f8acb8
Nate Bird <nate@lucidi.us>
2009-01-23 16:04:43 -0500

    Fixed specials from showing up on the homepage if they aren't active.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,20 +1,15 @@
 class AccountsController < ApplicationController
   
   def index
-    @accounts = Account.find(:all)
+    @accounts ||= Account.find(:all, :conditions => ['locations LIKE ?', "%#{params[:search]}%"])
   end
-
-  def auto_complete_for_account_locations
-    @accounts = Account.find(:all, :conditions => ['locations LIKE ?', "%#{params[:search]}%"])
-    render :partial => 'accounts'
-  end
-
   
   def show
-    @account = Account.find_by_accountlink(params[:accountlink])
-    @region = Region.find_by_abbreviation(params[:region].upcase)
+    @account ||= Account.find_by_accountlink(params[:accountlink])
+    @region ||= Region.find_by_abbreviation(params[:region].upcase)
     @testimonial ||= current_account.testimonials.find(:first, :order => APP_CONFIG[:random_query])
-    @special ||= current_account.specials.find(:first, :order => APP_CONFIG[:random_query])
+    @special ||= current_account.specials.find(:first, :conditions => [ "start <= ? and end >= ?", 
+              Date.today, Date.today ], :order => APP_CONFIG[:random_query])
 
     rescue
       redirect_to :action => "index"
