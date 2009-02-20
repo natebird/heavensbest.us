ef3cce8347ffb2e0d19d973c11c5c8c37820a175
Nate Bird <nate@lucidi.us>
2009-02-20 13:45:42 -0500

    Update specials and testimonials controllers to show services in the meta data.

--- app/controllers/specials_controller.rb
+++ app/controllers/specials_controller.rb
@@ -4,6 +4,7 @@ class SpecialsController < ApplicationController
   def index
     @specials = current_account.specials.find(:all, :conditions => [ "start <= ? and end >= ?", Date.today, Date.today ] )
     @testimonial ||= current_account.testimonials.find(:first, :order => APP_CONFIG[:random_query])
+    @services = @account.services.find(:all)
   end
 
 
