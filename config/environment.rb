ce7b338aa88441cff7f1db56f928b3ce64a698ec
Nate Bird <nate@lucidi.us>
2009-03-03 15:23:35 -0500

    Fix to RedCloth gem. 4.1.9 didn't work.

--- config/environment.rb
+++ config/environment.rb
@@ -33,7 +33,7 @@ Rails::Initializer.run do |config|
   # config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
   config.gem 'lockfile'
-  config.gem "RedCloth", :version => ">= 4.1.9", :source => "http://code.whytheluckystiff.net"
+  config.gem "RedCloth", :version => ">= 4.1.1", :source => "http://code.whytheluckystiff.net"
   
   # These cause problems with irb. Left in for reference
   # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
