52cea8032971cf02ac52968fdd85de6ad1ad0d70
Nate Bird <nate@lucidi.us>
2009-05-04 12:58:50 -0400

    Fix for showing specials in the services pages.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -13,10 +13,17 @@ class ServicesController < ApplicationController
   def show
     @service = current_account.services.find_by_servicelink(params[:id])
     @services = current_account.services.find(:all, :conditions => [ "active = ?", true ] )
+    @special ||= current_account.specials.find(:first, :conditions => [ "start <= ? and end >= ?", 
+              Date.today, Date.today ], :order => APP_CONFIG[:random_query])
     @testimonial ||= current_account.testimonials.find(:first, :order => APP_CONFIG[:random_query])
     rescue
       redirect_to area_path(@account.region.abbreviation.downcase, @account.accountlink)
       flash[:notice] = "No services found"
   end
+  
+  def set_current_tab
+    @current_tab = :services
+  end
+  
 
 end
