623f6c8c7eb46e3c9d54534d8f30da192bc7e97f
Nate Bird <nate@lucidi.us>
2009-06-18 11:45:30 -0400

    Allow service name permalinks to be updated.

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ActiveRecord::Base
   belongs_to :account
   belongs_to :photo
 
-  has_permalink :title, :servicelink, :scope => :account_id
+  has_permalink :title, :servicelink, :scope => :account_id, :update => true
   
   validates_length_of :title, :maximum=>28, :message=>"less than {{count}} or it won't fit"
   validates_presence_of :photo_id, :body, :title
