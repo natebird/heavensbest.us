f86b40434d8de04640dbf9cc2d8690fc0bf40940
Nate Bird <nate@lucidi.us>
2009-06-18 16:53:21 -0400

    A couple of tweaks to simplify error handling and moved the 404 page into the views folder for easier access.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,11 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation, :creditcard
-  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  rescue_from(ActiveRecord::RecordNotFound) { |e| render :file => 'errors/404', :status => :not_found }
+  rescue_from(NoMethodError) { |e| render :file => 'errors/404', :status => :not_found }
+  
   
   protected
-  
-  # Automatically respond with 404 for ActiveRecord::RecordNotFound
-  def record_not_found
-    render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
-  end
 
   # Find the current_account
   def current_account
@@ -41,7 +38,7 @@ class ApplicationController < ActionController::Base
 
   def not_authorized
     store_location
-    render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
+    render :file => 'errors/404', :status => 404
   end
 
 end
