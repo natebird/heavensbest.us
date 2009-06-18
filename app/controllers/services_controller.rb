0995216ca433330bc053fb6a706e71271b459ad8
Nate Bird <nate@lucidi.us>
2009-06-18 11:41:57 -0400

    Fixed problem with account not being active and throwing error. Still can't fix service not existing and properly redirecting.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,9 +4,11 @@ class ServicesController < ApplicationController
     @services = current_account.services.find(:all, :conditions => [ "active = ?", true ] )
     @region = @account.region.abbreviation.downcase
     redirect_to service_path(@region, @account.accountlink, @services.first.servicelink)
+    @current_tab = "services"
     rescue
-      redirect_to area_path(@account.region.abbreviation.downcase, @account.accountlink)
-      flash[:notice] = "No services found"
+      redirect_to root_path, :status => 301
+      flash[:notice] = "Area not found"
+      
   end
 
 
@@ -14,12 +16,13 @@ class ServicesController < ApplicationController
     @service = current_account.services.find_by_servicelink(params[:id])
     @services = current_account.services.find(:all, :conditions => [ "active = ?", true ] )
     @special ||= current_account.specials.find(:first, :conditions => [ "start <= ? and end >= ?", 
-              Date.today, Date.today ], :order => APP_CONFIG[:random_query])
+                 Date.today, Date.today ], :order => APP_CONFIG[:random_query])
     @testimonial ||= current_account.testimonials.find(:first, :order => APP_CONFIG[:random_query])
     @current_tab = "services"
     rescue
-      redirect_to area_path(@account.region.abbreviation.downcase, @account.accountlink)
-      flash[:notice] = "No services found"
+      redirect_to root_path, :status => 301
+      flash[:notice] = "Area not found"
+      
   end  
 
 end
