90db7ac6d87f8f680f021424d05580509f949074
Nate Bird <Nate@Nates-MacBook.local>
2010-02-01 16:51:27 -0500

    Removed code from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,6 @@ ActionController::Routing::Routes.draw do |map|
   # Home Page
   map.root :controller => "accounts", :action => "index"
 
-  # Restful Authentication Rewrites
-  # map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  # map.login '/login', :controller => 'sessions', :action => 'new'
-  # map.register '/register', :controller => 'admin/users', :action => 'create'
-  # map.activate '/signup/:activation_code', :controller => 'admin/users', :action => 'activate', :activation_code => nil
-  # map.forgot_password '/forgot_password', :controller => 'passwords', :action => 'new'
-  # map.change_password '/change_password/:reset_code', :controller => 'passwords', :action => 'reset'
-  # map.open_id_complete '/opensession', :controller => "sessions", :action => "create", :requirements => { :method => :get }
-  # map.open_id_create '/opencreate', :controller => "users", :action => "create", :requirements => { :method => :get }
-
   # Authlogic Routing
   map.resources :password_resets
   map.resource :user_session
@@ -21,17 +11,7 @@ ActionController::Routing::Routes.draw do |map|
   map.operator '/operator', :controller => 'user_sessions', :action => 'new'
   map.profile '/operator/profile/:id', :controller => 'admin/users', :action => 'edit'
   map.forgot_password '/forgot_password', :controller => 'password_resets', :action => 'new'
-  # map.change_password '/change_password/:reset_code', :controller => 'passwords', :action => 'reset'
   
-  # Restful Authentication Resources
-  # map.resources :passwords
-  # map.resource :session
-
-  # Billing and Subscription named routes
-  # map.cancel '/operator/areas/:id/cancel', :controller => 'operator/accounts', :action => 'cancel'
-  # map.billing '/operator/areas/:id/billing', :controller => 'operator/accounts', :action => 'billing'
-  # map.plan '/operator/areas/:id/plan', :controller => 'operator/accounts', :action => 'plan'
-
   # Operator Resources
   map.namespace :operator do |operator|
     operator.resources :accounts, :as => 'areas', :has_many => [:services, :specials, :testimonials, :frontpages]
@@ -54,7 +34,6 @@ ActionController::Routing::Routes.draw do |map|
   map.support '/support', :controller => 'pages', :action => 'show', :permalink => 'support'
   map.general_services '/services', :controller => 'pages', :action => 'show', :permalink => 'services'
   map.general_testimonials '/testimonials', :controller => 'pages', :action => 'show', :permalink => 'testimonials'
-
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
