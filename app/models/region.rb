995d29feee9830785aa2fb902535ceff4759e968
Nate Bird <natebird@gmail.com>
2011-05-11 09:38:30 -0400

    friendly_id

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,9 @@
 class Region < ActiveRecord::Base
   has_many :accounts
+  accepts_nested_attributes_for :accounts, :allow_destroy => false
+  
   belongs_to :country
 
-  # has_permalink :abbreviation
+  has_friendly_id :name, :use_slug => true
 
 end
